ebc1813dd305c5690f97d0bf7152f72dee64b499
rajat <rajat22999@gmail.com>
2024-08-26 15:30:15 +0530

    first commit

---
+++ src/constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconGitBranch,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://github.com/rajat22999",
+    label: "Github",
+    icon: IconGitBranch,
+  },
+  {
+    href: "https://www.linkedin.com/in/rajat22999/",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://x.com/rajat22999",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+];
